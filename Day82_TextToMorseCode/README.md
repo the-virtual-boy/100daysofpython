95026f73c44618bc3a3eb249aa881149575be8d6
Dsand <dsandrade9205@gmail.com>
2024-01-07 22:26:22 -0500

    day 82 initial requirements complete

---
+++ Day82_TextToMorseCode/README.md
@@ -0,0 +1,12 @@
+# Text to Morse Code translator
+
+This is a basic command line tool that takes input text and converts it to morse code
+
+
+## Usage
+
+1. Run the app with `python main.py` in the project folder.
+
+2. Enter a message to have converted to Morse Code and hit enter.
+
+3. Enjoy your morse code!
---
+++ Day82_TextToMorseCode/main.py
@@ -0,0 +1,46 @@
+# This application takes morse code or plain-text and translates it to the other
+#
+# Author: me ;)
+
+#imports
+
+
+#globals
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ',':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-', ' ':'    '}
+
+
+#functions
+
+## function in charge of converting text to morse code
+def convert(code):
+    global MORSE_CODE_DICT
+    if len(code) == 1:
+        return f"{MORSE_CODE_DICT[code[0].upper()]}"
+    else:
+        return f"{MORSE_CODE_DICT[code[0].upper()]}   {convert(code[1:])}"
+
+            
+## take input as text
+text_to_convert = input("Text to Convert: ")
+
+
+## convert it
+encoded_message = [convert(text_to_convert)]
+
+
+## output morse code
+print(f"Your message in Morse code is: {encoded_message}")
