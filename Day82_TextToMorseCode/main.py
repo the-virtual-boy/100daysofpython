273693d47c26c966f6b2b4df0e6f1c0f558c7e34
Dsand <dsandrade9205@gmail.com>
2024-01-09 13:19:58 -0500

    added code to be able to use command line arguments to run the applications silently, as an alternative to interacting with the app.

--- Day82_TextToMorseCode/main.py
+++ Day82_TextToMorseCode/main.py
@@ -3,7 +3,7 @@
 # Author: me ;)
 
 #imports
-
+import argparse
 
 #globals
 MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
@@ -20,7 +20,8 @@ MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
                     '7':'--...', '8':'---..', '9':'----.',
                     '0':'-----', ',':'--..--', '.':'.-.-.-',
                     '?':'..--..', '/':'-..-.', '-':'-....-',
-                    '(':'-.--.', ')':'-.--.-', '\'':'.----.', ' ':'/'}
+                    '(':'-.--.', ')':'-.--.-', '\'':'.----.',
+                      ' ':'/'}
 
 PLAIN_TEXT_DICT = {'.-': 'A', '-...': 'B', '-.-.': 'C',
                     '-..': 'D', '.': 'E', '..-.': 'F',
@@ -37,6 +38,14 @@ PLAIN_TEXT_DICT = {'.-': 'A', '-...': 'B', '-.-.': 'C',
                     '--..--': ', ', '.-.-.-': '.', '..--..': '?',
                     '-..-.': '/', '-....-': '-', '-.--.': '(',
                     '-.--.-': ')', '.----.':'\'', '/': ' '}
+
+# param arguments
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d", "--decode", help = "Sets decode mode")
+parser.add_argument("-e", "--encode", help = "Sets encode mode")
+
+args = parser.parse_args()
 
 #functions
 
@@ -59,16 +68,19 @@ def convert(code, mode):
             return f"{PLAIN_TEXT_DICT[code[0]]}{convert(code[1:], mode)}"
     else:
         return f"Error: incorrect value for mode. Use either E for encode or D for decode."
-
             
-## take input as text
-set_mode = input("Do you want to (E)ncode or (D)ecode a message?: ")[0].upper()
-text_to_convert = input("Text to Convert: ")
 
+if args.decode:
+    print(convert(args.decode, 'D'))
+elif args.encode:
+    print(convert(args.encode, 'E'))
+else:
+    ## get mode and message from user
+    set_mode = input("Do you want to (E)ncode or (D)ecode a message?: ")[0].upper()
+    text_to_convert = input("Text to Convert: ")
 
-## convert it
-encoded_message = [convert(text_to_convert, set_mode)]
+    ## convert it
+    encoded_message = [convert(text_to_convert, set_mode)]
 
-
-## output morse code
-print(f"Your message in Morse code is: {encoded_message}")
+    ## output morse code
+    print("Your message in Morse code is: ", encoded_message[0])
