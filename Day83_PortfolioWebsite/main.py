9299329c617c46a1f092025d522310b1c5d840ee
Dsand <dsandrade9205@gmail.com>
2024-01-12 08:29:24 -0500

    test

--- Day83_PortfolioWebsite/main.py
+++ Day83_PortfolioWebsite/main.py
@@ -1,8 +1,6 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
-from flask_ckeditor import CKEditor
-from flask_gravatar import Gravatar
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
