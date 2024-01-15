0c6c111053fafa7ecbd5071b818d3a90cff13e0f
Dsand <dsandrade9205@gmail.com>
2024-01-15 16:20:50 -0500

    initial commit with board class added

--- Day84_TicTacToeCLI/main.py
+++ Day84_TicTacToeCLI/main.py
@@ -3,93 +3,129 @@ from curses.textpad import Textbox, rectangle
 from time import sleep
 from math import ceil
 
-## GAME CONFIG GLOBALS
-BOARD_TOP = 0
-BOARD_BOTTOM = 4
-BOARD_LEFT = 1
-BOARD_RIGHT = 9
-X_SPACE = 2
-Y_SPACE = 4
+
 WINNING_COMBINATIONS = [['0-0', '0-1', '0-2'], ['1-0', '1-1', '1-2'], ['2-0', '2-1', '2-2'],
                         ['0-0', '1-0', '2-0'], ['0-1', '1-1', '2-1'], ['0-2', '1-2', '2-2'],
                         ['0-0', '1-1', '2-2'], ['2-0', '1-1', '0-2']]
+class GameBoard:
+    def __init__(self, window):
+        self.win = window
+        self.new_game()        
 
-def draw_board(win, board):
-        for i, row in enumerate(board):
-            win.addstr(i * 2, 0, "|".join(row))
+    def new_game(self, sym='X'):
+        self.board =  [["   ", "   ", "   "], ["   ", "   ", "   "], ["   ", "   ", "   "]]
+        self.player = sym
+        self.BOARD_TOP = 0
+        self.BOARD_BOTTOM = 4
+        self.BOARD_LEFT = 0
+        self.BOARD_RIGHT = 10
+        self.X_SPACE = self.BOARD_BOTTOM // 2
+        self.Y_SPACE = (self.BOARD_RIGHT - 2) // 2
+        self.set_center()
+        self.display_turn()
+
+
+    def draw_board(self):
+        for i, row in enumerate(self.board):
+            self.win.addstr(i * 2, 0, "|".join(row))
             if i<2:
-                win.addstr(i * 2 + 1, 0, "-" * 11)
+                self.win.addstr(i * 2 + 1, 0, "-" * (self.BOARD_RIGHT + 1))
+
+    def move_player(self, dir):
+        if dir == 0 and self.X_LOC > self.BOARD_TOP:
+            self.X_LOC -= self.X_SPACE
+        elif dir == 1 and self.Y_LOC < self.BOARD_RIGHT - 1:
+            self.Y_LOC += self.Y_SPACE
+        elif dir == 2 and self.X_LOC < self.BOARD_BOTTOM:
+            self.X_LOC += self.X_SPACE
+        elif dir == 3 and self.Y_LOC > self.BOARD_LEFT + 1:
+            self.Y_LOC -= self.Y_SPACE
+
+    def make_move(self):
+        if self.board[self.X_LOC // self.X_SPACE][self.Y_LOC // self.Y_SPACE] == "   ":
+            self.board[self.X_LOC // self.X_SPACE][self.Y_LOC // self.Y_SPACE] = f" {self.player} "
 
 
-def check_win(win, board):
-    for list in WINNING_COMBINATIONS:
-        print(list)
-        check = [ board[int(iter[0])][int(iter[2])] for iter in list ]
-        print('check:\n' + ':'.join(check).replace(' ', '') + '\n+++++++++++++++++')
-        if ''.join(check).replace(' ', '') == 'XXX' or ''.join(check).replace(' ', '') == 'OOO':
-            win.clear()
-            draw_board(win, board)
-            win.addstr(BOARD_BOTTOM + 2, BOARD_LEFT, f'{check[0]} WINS!')
-            return True
+    def change_player(self):
+        self.player = 'X' if self.player == 'O' else 'O'
 
-def display_turn(win, sym):    
-    win.addstr(BOARD_BOTTOM + 2, BOARD_LEFT, f"It's {sym} turn")
-
-
-def main(stdscr, game=0):
-    board =  [["   ", "   ", "   "], ["   ", "   ", "   "], ["   ", "   ", "   "]]
-    curses.curs_set(True)
-    y, x = 5, 2
-    current_symbol = 'O' if game % 2 == 1 else 'X'
-    display_turn(stdscr, current_symbol)
+    def check_win(self):
+        for list in WINNING_COMBINATIONS:
+            check = [self.board[int(iter[0])][int(iter[2])] for iter in list ]
+            if ''.join(check).replace(' ', '') == 'XXX' or ''.join(check).replace(' ', '') == 'OOO':
+                self.win.clear()
+                self.draw_board()
+                self.win.addstr(self.BOARD_BOTTOM + 2, self.BOARD_LEFT, f'{check[0]} WINS!')
+                return True
+        return False
     
+    def set_cursor(self):
+        self.win.move(self.X_LOC, self.Y_LOC)
+
+    def set_center(self):
+        self.X_LOC = self.BOARD_BOTTOM // 2
+        self.Y_LOC = self.BOARD_RIGHT // 2
+
+    def display_turn(self):    
+        self.win.addstr(self.BOARD_BOTTOM + 2, self.BOARD_LEFT, f"It's {self.player} turn")
+
+    def print_win(self):
+        self.win.addstr(self.BOARD_BOTTOM + 4, self.BOARD_LEFT, f"{self.player}WINS! ")
+
+    def prompt_newgame(self):
+        self.win.addstr(self.BOARD_BOTTOM + 4, self.BOARD_LEFT, f"Hit ENTER to play again, hit anything else to quit")
+        key = self.win.getch()
+        if key == 10:            
+            ## needed for when computer is added?
+            # self.win.addstr(self.BOARD_BOTTOM + 6, self.BOARD_LEFT, f"Great, you'll start as {'X' if self.player == 'O' else 'O'} this time!")
+            self.win.addstr(self.BOARD_BOTTOM + 6, self.BOARD_LEFT, f"Great! Make sure to swap players!")
+            self.win.refresh()
+            for x in (1,2,3):
+                self.win.addstr('.')
+                self.win.refresh()
+                sleep(1)
+            self.win.clear()
+            return True
+        return False
+
+    def new_player(self):
+        return ('X' if self.player == 'O' else 'O')            
+
+def main(stdscr):
+    board = GameBoard(stdscr)
+
     while True:
-        
-        draw_board(stdscr, board)        
-        stdscr.move(x,y)
+
+        board.draw_board()
+        board.set_cursor()
         key = stdscr.getch()
-
-        if key == curses.KEY_UP and x > BOARD_TOP:
-            x -= X_SPACE            
-        elif key == curses.KEY_DOWN and x < BOARD_BOTTOM:
-            x += X_SPACE
-
-        elif key == curses.KEY_LEFT and y > BOARD_LEFT:
-            y -= Y_SPACE
-
-        elif key == curses.KEY_RIGHT and y < BOARD_RIGHT:
-            y += Y_SPACE
-
+        if key == curses.KEY_UP:
+            board.move_player(0)
+        elif key == curses.KEY_RIGHT:
+            board.move_player(1)       
+        elif key == curses.KEY_DOWN:
+            board.move_player(2)
+        elif key == curses.KEY_LEFT:
+            board.move_player(3)
         elif key == 10:
-            if board[x // X_SPACE][y // Y_SPACE] == "   ":
-                board[x // X_SPACE][y // Y_SPACE] = f" {current_symbol} "
-                print(*board, sep='\n')
-                print('~~~~~~~~~~~~~~~~~')
-                if check_win(stdscr, board):
-                    draw_board(stdscr, board)
-                    
+            board.make_move()
+            if board.check_win():
+                board.print_win()
+                stdscr.refresh()
+                if board.prompt_newgame():
+                    board.new_game()
+                else:
                     break
-                current_symbol = "O" if current_symbol == "X" else "X"
-                display_turn(stdscr, current_symbol)
-
+            else:
+                board.change_player()
+                board.display_turn()
         elif key == curses.ascii.ESC:
             exit()
-        
-        check_win(stdscr, board)
 
+                
+        
         stdscr.refresh()
-    game += 1
-    stdscr.addstr(BOARD_BOTTOM + 4, BOARD_LEFT, f"Hit ENTER to play again, hit anything else to quit")
-    key = stdscr.getch()
-    if key == 10:
-        stdscr.addstr(BOARD_BOTTOM + 6, BOARD_LEFT, f"Great, you'll start as {'O' if game % 2 == 1 else 'X'} this time!")
-        stdscr.refresh()
-        for x in (1,2,3):
-            stdscr.addstr('.')
-            stdscr.refresh()
-            sleep(1)
-        stdscr.clear()
-        main(stdscr, game)
-    
+        
+
 
 curses.wrapper(main)
